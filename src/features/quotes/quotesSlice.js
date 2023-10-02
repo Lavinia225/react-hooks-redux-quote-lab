23e6438188270cfdff592173118a829281f07dec
Luna Seifert <thepuddingdeity@gmail.com>
2023-10-02 18:44:11 -0500

    commit tree fee229694926d3d1901cd10807845bf4a20fe54a

--- src/features/quotes/quotesSlice.js
+++ src/features/quotes/quotesSlice.js
@@ -4,6 +4,64 @@
 // Reducer
 const initialState = [];
 
+export function addQuote(quote){
+  return {
+    type: "quotes/add",
+    payload: quote
+  }
+}
+
+export function removeQuote(quoteId){
+  return {
+    type: "quotes/remove",
+    payload: quoteId
+  }
+}
+
+export function upvoteQuote(quoteId){
+  return {
+    type: "quotes/upvote",
+    payload: quoteId
+  }
+}
+
+export function downvoteQuote(quoteId){
+  return {
+    type: "quotes/downvote",
+    payload: quoteId
+  }
+}
+
 export default function quotesReducer(state = initialState, action) {
-  return state;
+  switch (action.type){
+    case 'quotes/add':
+      return [...state, action.payload]
+    case 'quotes/remove':
+      return state.filter(quote => quote.id !== action.payload)
+    case 'quotes/upvote':
+      return state.map(incrementVote)
+    case 'quotes/downvote':
+      return state.map(decrementVote)
+    default:
+      return state;
+  }
+
+  function incrementVote(quote){
+    if (quote.id === action.payload){
+      quote.votes += 1
+      return quote
+    }
+    else{
+      return quote
+    }
+  }
+
+  function decrementVote(quote){
+    if (quote.id === action.payload){
+      if (quote.votes !== 0){
+        quote.votes -= 1
+      }
+      return quote
+    }
+  }
 }
